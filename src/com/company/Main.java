b54c1c74c8083e1579abd07ba1917e82d856e5fd
NilsHulzebosch <macnilsmac@hotmail.com>
2016-02-04 19:58:37 +0100

    2 added

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,9 +11,22 @@ public class Main {
         }
         System.out.println("ey print dit gewoon");
 
-        // tileBoard with cars on it
-        int[][] tileBoard = new int[10][10];
 
-        
+        // car lengths
+        int smallCar = 2;
+        int bigCar = 3;
+
+        // tileBoard with cars on it
+        int[][] tileBoard = new int[6][6];
+        tileBoard[3][2] = 5;
+        int redCar = tileBoard[3][2];
+        System.out.println(redCar);
+
+
+        // show available spots on tileboard
+        boolean[][] isAvailable = new boolean[6][6];
+
+
+
     }
 }
