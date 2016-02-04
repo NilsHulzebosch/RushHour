8b1da173655c0ce1d88dc7a76aa05724b6286eb1
Sjors Witteveen <sjors@jores.nl>
2016-02-04 19:20:47 +0100

    dit is een update

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,8 +3,9 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	 System.out.println("Hello world");
+        String text = "Hello world";
+        for (int i = 0; i < text.length(); i++) {
+            System.out.println("Hello world");
+        }
     }
-
-
 }
