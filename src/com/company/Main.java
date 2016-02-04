19b173bbc0501893dad8bbeee59c41ede426437d
Sjors Witteveen <sjors@jores.nl>
2016-02-04 19:34:37 +0100

    update

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         String text = "Hello world";
         for (int i = 0; i < text.length(); i++) {
-            System.out.println("Hello world");
+            System.out.println(text.charAt(i));
         }
         System.out.println("ey print dit gewoon");
 
