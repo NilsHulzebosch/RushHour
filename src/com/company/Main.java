7b7ad6ecf862bb98e738b4c0a06d0b673c133ce3
Nils <macnilsmac@hotmail.com>
2016-02-15 20:28:04 +0100

    2 added

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,38 @@ package com.company;
 
 public class Main {
 
+    // board size
+    static final int BOARD_SIZE = 6;
+
+    private static String horizontal3length = "hor3";
+    private static String horizontal2length = "hor2";
+    private static String vertical3length = "ver3";
+    private static String vertical2length = "ver2";
+
+    // tileBoard with cars on it
+    private static String[][] tileBoard = new String[BOARD_SIZE][BOARD_SIZE];
+
+
+/*
+    ["hor3"][][][][][]
+    [][][][][][]
+    [][][][][][]
+    [][][]["ver2"][][]
+    ["ver2"][][][][][]
+    [][][][][][]
+*/
     public static void main(String[] args) {
+
+        tileBoard[0][0] = horizontal3length;
+        tileBoard[0][3] = vertical2length;
+        tileBoard[0][4] = vertical2length;
+
+        for(int i = 0; i < 6; i++) {
+            for(int j = 0; j < 6; j++) {
+                System.out.println(tileBoard[i][j]);
+            }
+        }
+
         String text = "Hello world";
         String text_2 = "";
         for (int i = 0; i < text.length(); i++) {
@@ -14,11 +45,8 @@ public class Main {
         int smallCar = 2;
         int bigCar = 3;
 
-        // tileBoard with cars on it
-        int[][] tileBoard = new int[6][6];
-        tileBoard[3][2] = 5;
-        int redCar = tileBoard[3][2];
-        System.out.println(redCar);
+
+        //System.out.println("");
 
 
         // show available spots on tileboard
