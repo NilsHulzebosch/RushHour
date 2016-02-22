34120b551f6e9c7e0cdb92089d0fb1fd98284002
Sjors Witteveen <sjors@jores.nl>
2016-02-23 00:28:39 +0100

    update

--- src/com/company/Grid.java
+++ src/com/company/Grid.java
@@ -5,6 +5,34 @@ package com.company;
  */
 public class Grid {
 
-    private boolean[][] array = new boolean[6][6];
+    private int size;
+    private int[][] array;
 
+    public Grid(int size) {
+        this.size = size;
+        array = new int[size][size];
+    }
+
+    public void addNumber(int x, int y, int number){
+        array[x][y] = number;
+    }
+
+    public void moveRight(int x, int y) {
+        array[x + 1][y + 1] = array[x][y];
+        array[x][y] = 0;
+    }
+
+    public void moveLeft(int x, int y) {
+        array[x - 1][y - 1] = array[x][y];
+        array[x][y] = 0;
+    }
+
+    public void printGrid() {
+        for (int i = 0; i < size; i++){
+            for (int j = 0; j < size; j++){
+                System.out.print(array[j][i] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
