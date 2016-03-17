26733a14ec713e7a852fae1ccba16009f3967432
Nils <macnilsmac@hotmail.com>
2016-03-17 09:58:30 +0100

    seventh puzzle added

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -114,7 +114,67 @@ public class Main {
         grid.addVehicle(true, 2, 7, 8);
     }
 
-    // animate the moves
+    private static void addSeventhPuzzle() {
+        // add red hor. car (so it will get number 1)
+        grid.addVehicle(true, 2, 2, 5);
+
+        grid.addVehicle(false, 2, 0, 0);    // turquoise ver. car
+        grid.addVehicle(false, 2, 6, 0);    // blue ver. car
+        grid.addVehicle(true, 3, 7, 0);     // purple hor. truck
+        grid.addVehicle(true, 2, 10, 0);    // cyan hor. car
+
+        grid.addVehicle(false, 2, 5, 1);    // cyan ver. car
+        grid.addVehicle(false, 2, 10, 1);   // orange ver. car
+        grid.addVehicle(false, 2, 11, 1);   // blue ver. car
+
+        grid.addVehicle(true, 3, 0, 2);     // light-yellow hor. truck
+        grid.addVehicle(true, 2, 3, 2);     // orange hor. car
+        grid.addVehicle(false, 3, 6, 2);    // dark-yellow ver. truck
+        grid.addVehicle(true, 2, 7, 2);     // turquoise hor. car
+
+        grid.addVehicle(false, 3, 0, 3);    // purple ver. truck
+        grid.addVehicle(false, 3, 1, 3);    // dark-yellow ver. truck
+        grid.addVehicle(false, 2, 5, 3);    // turquoise ver. car
+        grid.addVehicle(true, 2, 7, 3);     // orange hor. car
+        grid.addVehicle(true, 2, 9, 3);     // cyan hor. car
+
+        grid.addVehicle(true, 3, 2, 4);     // grey hor. truck
+        grid.addVehicle(true, 3, 7, 4);     // purple hor. truck
+
+        grid.addVehicle(true, 2, 4, 5);     // orange ver. car
+        grid.addVehicle(true, 2, 5, 5);     // cyan ver. car
+
+        grid.addVehicle(true, 3, 0, 6);     // grey hor. truck
+        grid.addVehicle(false, 2, 3, 6);    // cyan ver. car
+        grid.addVehicle(false, 3, 6, 6);    // light-yellow ver. truck
+        grid.addVehicle(false, 2, 7, 6);    // blue ver. car
+        grid.addVehicle(false, 2, 9, 6);    // cyan ver. car
+        grid.addVehicle(true, 2, 10, 6);    // turquoise hor. car
+
+        grid.addVehicle(true, 3, 0, 7);     // light-yellow hor. truck
+        grid.addVehicle(false, 2, 4, 7);    // turquoise hor. car
+        grid.addVehicle(false, 2, 10, 7);   // blue hor. car
+
+        grid.addVehicle(true, 2, 0, 8);     // orange hor. car
+        grid.addVehicle(false, 2, 2, 8);    // turquoise ver. car
+        grid.addVehicle(true, 3, 3, 8);     // purple hor. truck
+        grid.addVehicle(true, 3, 7, 8);     // dark-yellow hor. truck
+        grid.addVehicle(false, 2, 11, 8);   // turquoise ver. car
+
+        grid.addVehicle(true, 3, 3, 9);     // light-yellow hor. truck
+        grid.addVehicle(false, 3, 6, 9);    // grey ver. truck
+        grid.addVehicle(true, 2, 8, 9);     // turquoise hor. car
+        grid.addVehicle(false, 3, 10, 9);   // purple ver. truck
+
+        grid.addVehicle(false, 2, 9, 10);   // cyan ver. car
+        grid.addVehicle(false, 2, 11, 10);  // cyan ver. car
+
+        grid.addVehicle(true, 2, 1, 11);    // turquoise hor. car
+        grid.addVehicle(true, 3, 3, 11);    // dark-yellow hor. truck
+        grid.addVehicle(true, 2, 7, 11);    // blue hor. car
+    }
+
+        // animate the moves
     private static void visualize() {
         wipeScreen();
         grid.printGrid();
